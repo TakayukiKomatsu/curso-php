b28dd8c2e5253010c6a95607472df92ad3368d1c
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-05 01:33:13 -0300

    add exercise boilerplate

---
+++ exercise.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <title>Exercício</title>
+</head>
+
+<body>
+  <header class="header">
+    <h1>Curso PHP</h1>
+    <h2>Visualização do Exercício</h2>
+  </header>
+  <main class="main">
+    <div class="content">
+
+    </div>
+  </main>
+  <footer class="footer">COD3R & ALUNOS ©
+    <?=
+    date('Y');
+    ?>
+  </footer>
+</body>
+
+</html>
