dc9af69d015b3e7dd50539ca6bf564158e4c65c4
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-05 01:38:50 -0300

    add exercise. grid

--- exercise.php
+++ exercise.php
@@ -6,10 +6,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="assets/css/exercises.css" />
   <title>Exercício</title>
 </head>
 
-<body>
+<body class="exercise">
   <header class="header">
     <h1>Curso PHP</h1>
     <h2>Visualização do Exercício</h2>
