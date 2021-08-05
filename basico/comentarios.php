c9898796e06db37f300a80331d5490129286fd7a
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-05 12:50:13 -0300

    add basico - comentarios.php

---
+++ basico/comentarios.php
@@ -0,0 +1,29 @@
+<div class="title">
+  Comentários PHP
+</div>
+
+<?php
+
+// echo 'Estou no PHP';
+
+# echo 'Estou no PHP';
+
+
+//
+?>
+<p>Depois do primeiro bloco!</p>
+<!-- <p>Depois do primeiro bloco!</p> -->
+
+<?php
+
+
+
+/*
+  Linha 1 do comentário
+  Linha 2 do comentário
+  Linha 3 do comentário
+
+  Mais forte do que o fechamento do bloco
+  Comentários em PHP não serão irão para o Browser
+*/
+?>
