630c389b7060096637caf1e8b0c8e0099db35b09
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-08 11:46:07 -0300

    add tipos - int

---
+++ tipos/int.php
@@ -0,0 +1,23 @@
+<div class="title">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11); /* Int(11) */
+echo '<br>';
+
+/* Constantes disponíveis no PHP */
+
+/* Valores extremos dos inteiros no PHP */
+echo PHP_INT_MAX; /* Depende da arquitetura do PC */
+echo '<br>';
+
+echo PHP_INT_MIN; /* Depende da arquitetura do PC */
+echo '<br>';
+
+/* Bases Numéricas */
+echo 011; /* 0 => identifica que o numero é octal | Base Octal*/
+echo 0b11; /* b => identifica que o numero é binário | Base Binário*/
+echo 0x11; /* x => identifica que o numero é Hexadecimal | Base Hexadecimal*/
+/* Exemplo */
